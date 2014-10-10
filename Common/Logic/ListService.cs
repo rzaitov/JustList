c290e73829eaa3268018c147a679ff628bc00381
Rustam Zaitov <rustam.zaitov@xamarin.com>
2014-10-10 23:33:12 +0400

    add persistence for app session

--- Common/Logic/ListService.cs
+++ Common/Logic/ListService.cs
@@ -5,9 +5,12 @@ namespace Common
 {
 	public class ListService
 	{
-		public IList<List> FetchLists()
+		readonly IList<List> list;
+		readonly Dictionary<Guid, List<Item>> storage;
+
+		public ListService()
 		{
-			return new List<List> {
+			list = new List<List> {
 				new List {
 					Name = "First",
 					Color = ListColor.Red,
@@ -19,37 +22,44 @@ namespace Common
 					Id = Guid.Parse("c8fd7a85-98f5-40d8-8002-a6a298397ee2")
 				}
 			};
+
+			storage = new Dictionary<Guid, List<Item>> { { Guid.Parse ("52bf9a8a-9b52-4fba-8cd0-22dbe62e28df"), new List<Item> {
+						new Item {
+							IsComplete = false,
+							Text = "Apple"
+						},
+						new Item {
+							IsComplete = true,
+							Text = "Banana"
+						}
+					}
+				}, { Guid.Parse ("c8fd7a85-98f5-40d8-8002-a6a298397ee2"), new List<Item> {
+						new Item {
+							IsComplete = false,
+							Text = "Cycle"
+						},
+						new Item {
+							IsComplete = true,
+							Text = "Car"
+						},
+						new Item {
+							IsComplete = true,
+							Text = "Train"
+						}
+					}
+				}
+			};
+		}
+
+		public IList<List> FetchLists()
+		{
+			return list;
 		}
 
 		public IList<Item> FetchItems(Guid listId)
 		{
-			if (listId == Guid.Parse ("52bf9a8a-9b52-4fba-8cd0-22dbe62e28df")) {
-				return new List<Item> {
-					new Item {
-						IsComplete = false,
-						Text = "Apple"
-					},
-					new Item {
-						IsComplete = true,
-						Text = "Banana"
-					}
-				};
-			} else if (listId == Guid.Parse ("c8fd7a85-98f5-40d8-8002-a6a298397ee2")) {
-				return new List<Item> {
-					new Item {
-						IsComplete = false,
-						Text = "Cycle"
-					},
-					new Item {
-						IsComplete = true,
-						Text = "Car"
-					},
-					new Item {
-						IsComplete = true,
-						Text = "Train"
-					}
-				};
-			}
+			if (storage.ContainsKey (listId))
+				return storage [listId];
 
 			return null;
 		}
